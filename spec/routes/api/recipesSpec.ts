ce2904e1ef051930b03ba7e34b4bdb481fb7d7ed
Datt Goswami <dattgoswami@gmail.com>
2021-11-06 19:58:33 -0600

    added test cases and changed where errors were being handled and did some refactoring

--- spec/routes/api/recipesSpec.ts
+++ spec/routes/api/recipesSpec.ts
@@ -1,5 +1,5 @@
 import supertest from 'supertest';
-import app from '../../../src/index';
+import app from '../../../src/server';
 
 const request = supertest(app);
 
@@ -15,5 +15,27 @@ describe('recipes endpoint response test suite', () => {
     const chaiRecipe = { details: { ingredients: [[Array]], numSteps: 4 } };
     expect(res.status).toBe(200);
     expect(res.body.details.numSteps).toEqual(chaiRecipe.details.numSteps);
+  });
+  it('post recipe creation endpoint', async () => {
+    const res = await request.post('/recipes').send({
+      name: 'chai',
+      ingredients: [
+        '400mL water',
+        '100mL milk',
+        '5g chai masala',
+        '2 tea bags or 20 g loose tea leaves'
+      ],
+      instructions: [
+        'Heat water until 80 C',
+        'Add milk, heat until 80 C',
+        'Add tea leaves/tea bags, chai masala; mix and steep for 3-4 minutes',
+        'Remove mixture from heat; strain and enjoy'
+      ]
+    });
+    expect(res.status).toBe(400);
+    console.log(res.body);
+    expect(res.body).toEqual({
+      error: 'Recipe already exists'
+    });
   });
 });
